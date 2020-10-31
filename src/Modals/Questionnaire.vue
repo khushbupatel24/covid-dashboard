9d198d7e58fae05a6f818771fe5b447e27c5dc0e
PURUSHOTTAM BHITE <bhite@uwindsor.ca>
2020-11-01 00:31:05 +0530

    test

--- src/Modals/Questionnaire.vue
+++ src/Modals/Questionnaire.vue
@@ -1,111 +1,105 @@
 <template>
-    <survey :survey="survey" />
+  <survey :survey="survey" />
 </template>
-
-//  <script src="https://surveyjs.azureedge.net/1.8.12/survey.vue.min.js" type="text/javascript"> </script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.18/vue.min.js" type="text/javascript"> </script>
+ 
 <script>
-    import * as Survey from "survey-vue";
-    import "survey-vue/survey.css";
-    import "./quesstyle.css";
-    Survey.StylesManager.applyTheme("darkblue");
+import * as Survey from "survey-vue";
+import "survey-vue/survey.css";
+import "./quesstyle.css";
+Survey.StylesManager.applyTheme("darkblue");
 
-    export default {
-        name: "questionnaire",
-        data() {
-            const json = {
-        title: "Medical Diagnosis",
-        showProgressBar: "bottom",
-        firstPageIsStarted: true,
-        startSurveyText: "Start Diagnosis",
-        pages: [
+export default {
+  name: "questionnaire",
+  data() {
+    const json = {
+      title: "Medical Diagnosis",
+      showProgressBar: "bottom",
+      firstPageIsStarted: true,
+      startSurveyText: "Start Diagnosis",
+      pages: [
+        {
+          questions: [
             {
-            questions: [
-                {
-                    type: "html",
-                    html: "You are about to start your Medical diagnosis to detect Coronavirus symptoms.<br/>Information is still evolving.** Allergies, colds and flus can all trigger asthama, which can lead to shortening of breathe<br/>COVID-19 is the only one associated with shortness of breath on its own. Sources: Asthama and allergy foundation of America, World Health Organisation, Centres of disease control and Prevention<br/>Please click on <b>'Start'</b> button when you are ready."
-                }
-            ]
-            }, {
-            questions: [
-                {
-                    type: "radiogroup",
-                    name: "cold",
-                    title: "Are you experiencing cough?",
-                    choices: [
-                        "Yes, Dry", "Yes, but Mild", "Yes, Wet", "No"
-                    ],
-                    correctAnswer: "",
-                    covid: "Yes, Dry",
-                    cold:"Yes, but Mild",
-                    flu:"Yes, Dry",
-                    allergy:"Yes, Dry"
-                }
-            ]
-            }, {
-            questions: [
-                {
-                    type: "radiogroup",
-                    name: "shortnessofbreath",
-                    title: "Do you have shortness of breath",
-                    choicesOrder: "random",
-                    choices: [
-                        "Yes", "No", "Sometimes"
-                    ],
-                    correctAnswer: "",
-                    covid: "Sometimes",
-                    cold:"No",
-                    flu:"No",
-                    allergy:"No"
-
-                }
-            ]
-         }, {
-            questions: [
-                {
-                    type: "radiogroup",
-                    name: "sneeze",
-                    title: "Do you often sneeze?",
-                    choicesOrder: "random",
-                    choices: [
-                        "Yes" , "No"
-                    ],
-                    correctAnswer: "",
-                    covid: "No",
-                    cold:"Yes",
-                    flu:"No",
-                    allergy:"Yes"
-                }
-            ]
+              type: "html",
+              html:
+                "You are about to start your Medical diagnosis to detect Coronavirus symptoms.<br/>Information is still evolving.** Allergies, colds and flus can all trigger asthama, which can lead to shortening of breathe<br/>COVID-19 is the only one associated with shortness of breath on its own. Sources: Asthama and allergy foundation of America, World Health Organisation, Centres of disease control and Prevention<br/>Please click on <b>'Start'</b> button when you are ready.",
+            },
+          ],
         },
         {
-            questions: [
-                {
-                    type: "radiogroup",
-                    name: "runningnose",
-                    title: "Do you have runny or stuffy nose?",
-                    choicesOrder: "random",
-                    choices: [
-                       "Rarely" , "Yes" , "Sometimes","No"
-                    ],
-                    correctAnswer: "",
-                    covid: "Rarely",
-                    cold:"Yes",
-                    flu:"Sometimes",
-                    allergy:"Yes"
-                }
-            ]
+          questions: [
+            {
+              type: "radiogroup",
+              name: "cold",
+              title: "Are you experiencing cough?",
+              choices: ["Yes, Dry", "Yes, but Mild", "Yes, Wet", "No"],
+              correctAnswer: "",
+              covid: "Yes, Dry",
+              cold: "Yes, but Mild",
+              flu: "Yes, Dry",
+              allergy: "Yes, Dry",
+            },
+          ],
         },
-    ],
-    completedHtml: "<h4>Your symptoms indicate you have <b>{correctedAnswers}</b> percent chances of COVID19 symptoms</b>.</h4>"
-};
-
-const survey = new Survey.Model(json);
-            return {
-                survey: survey
-            };
-        }
+        {
+          questions: [
+            {
+              type: "radiogroup",
+              name: "shortnessofbreath",
+              title: "Do you have shortness of breath",
+              choicesOrder: "random",
+              choices: ["Yes", "No", "Sometimes"],
+              correctAnswer: "",
+              covid: "Sometimes",
+              cold: "No",
+              flu: "No",
+              allergy: "No",
+            },
+          ],
+        },
+        {
+          questions: [
+            {
+              type: "radiogroup",
+              name: "sneeze",
+              title: "Do you often sneeze?",
+              choicesOrder: "random",
+              choices: ["Yes", "No"],
+              correctAnswer: "",
+              covid: "No",
+              cold: "Yes",
+              flu: "No",
+              allergy: "Yes",
+            },
+          ],
+        },
+        {
+          questions: [
+            {
+              type: "radiogroup",
+              name: "runningnose",
+              title: "Do you have runny or stuffy nose?",
+              choicesOrder: "random",
+              choices: ["Rarely", "Yes", "Sometimes", "No"],
+              correctAnswer: "",
+              covid: "Rarely",
+              cold: "Yes",
+              flu: "Sometimes",
+              allergy: "Yes",
+            },
+          ],
+        },
+      ],
+      completedHtml:
+        " symptoms indicate you have <b>{correctedAnswers}</b> percent chances of COVID19 symptoms</b>.",
     };
+
+    const survey = new Survey.Model(json);
+    return {
+      survey: survey,
+    };
+  },
+};
 </script>
 
 <style scoped>
