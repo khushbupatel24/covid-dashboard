1713676dd587ea334e16ebfc5335b34d37f21a1f
Keerthana-Tummala <tummala.keerthana9@gmail.com>
2020-10-31 19:11:48 +0530

    Register page Image. Changes made in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
+ <div id="app" class="h-100">
+    <div class="container-fluid h-100">
+      <div class="row h-100">
+        <div class="col-md-3 vue-bg h-100 d-flex justify-content-center align-items-center">
+          <img alt="Vue logo" src="./assets/Red_leaf.jpg" width="100">
+        </div>
+        <div class="col-md-9 h-100 d-flex justify-content-center align-items-center">
+          <div class="col-md-8 rounded px-5 py-4 shadow bg-white text-left"><login /></div>
+        </div>
+      </div>
+    </div>
+  </div>
   <router-view></router-view>
 
+ 
 </template>
 
 <script>
@@ -29,3 +42,15 @@ import Questionnaire from "./Modals/Questionnaire";
     };
 </script>
 
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #502c2c;
+}
+.vue-bg {
+  background: #23f1ac;
+}
+</style>
